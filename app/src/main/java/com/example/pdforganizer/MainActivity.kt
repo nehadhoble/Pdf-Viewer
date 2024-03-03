f74ec8cbdc19715a6c2ac0bc672135dc1ab19817
Neha Dhoble <nehagdhoble19@gmail.com>
2024-03-03 17:48:06 +0530

    created pdfViewer and added FAB to load pdf in viewer

--- app/src/main/java/com/example/pdforganizer/MainActivity.kt
+++ app/src/main/java/com/example/pdforganizer/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.pdforganizer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.pdforganizer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var  binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.floatingActionButton.setOnClickListener {
+            launcher.launch("application/pdf")
+        }
+    }
+
+    private val launcher =  registerForActivityResult(
+        ActivityResultContracts.GetContent()
+    ){ uri ->
+        uri?.let {
+            binding.pdfViewer.fromUri(it).load()
+        }
     }
 }
